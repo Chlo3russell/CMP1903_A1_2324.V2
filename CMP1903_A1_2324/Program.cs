1699f5f70692aa72c49ef9799f75d74afb454e64
Chloe Russell <chloerussell2017@gmail.com>
2024-02-16 20:04:56 +0000

    Finished the Game class and added the testing class
    Updated files

---
+++ CMP1903_A1_2324/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A1_2324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the class Game
+            Game newGame = new Game();
+            // Using the method DiceRolling to roll the "dice"
+            newGame.DiceRolling();
+            // Using the method KeepRolling to create a continuous rolling loop
+            newGame.KeepRolling();
+            // Tells you how many of each no was rolled
+            newGame.DisplayAmounts();
+
+            // Testing the two classes Game and Die
+            Testing test1 = new Testing();
+            test1.DieTesting();
+            test1.GameTesting();
+        }
+    }
+}
